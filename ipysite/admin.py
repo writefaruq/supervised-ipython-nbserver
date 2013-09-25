5d20da3b462ad3db31b33a4d5f8d1e1cc578b7f8
Faruque Sarker <f.sarker@ucl.ac.uk>
2013-09-25 10:08:58 +0100

    Added admin actions: enable/disable nbserver access

--- ipysite/admin.py
+++ ipysite/admin.py
@@ -10,10 +10,33 @@ class UserProfileInline(admin.StackedInline):
     model = UserProfile
     can_delete = False
     verbose_name_plural = 'profile'
+    
 
 # Define a new User admin
 class UserAdmin(UserAdmin):
     inlines = (UserProfileInline, )
+    
+    actions = ['enable_nbserver_access', 'disable_nbserver_access']
+    
+    
+    def _change_access(self, queryset, enable):
+        for obj in queryset:
+            user_profiles = UserProfile.objects.filter(user=obj)
+            if user_profiles:
+                user_profile = user_profiles[0]
+                user_profile.access_enabled = enable
+                user_profile.save() 
+    
+    def enable_nbserver_access(self, request, queryset):
+        """ Enable notebook server access the selected users"""
+        self._change_access(queryset, enable=True)
+    enable_nbserver_access.short_description = "Enable Notebook server access for selected users"
+
+    def disable_nbserver_access(self, request, queryset):
+        """ Disable notebook server access the selected users"""
+        self._change_access(queryset, enable=False)
+    disable_nbserver_access.short_description = "Disable Notebook server access for selected users"
+
 
 # Re-register UserAdmin
 admin.site.unregister(User)
